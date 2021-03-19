9d715b4872b36f7f8aea0affce4097ab6d5babd3
veronicahwambui <njengaveronicahwambui@gmail.com>
2021-03-19 21:00:30 +0300

    first commit

---
+++ sharon.js
@@ -0,0 +1,16 @@
+function name() {
+    var a="Shadya"
+    var b="Pamera"
+    
+    function identity() {
+        var c=a +" and " + b + " are friends ";
+        console.log(c);
+        
+        
+    }
+        
+identity();
+
+    
+}
+name()
